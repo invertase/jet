f7ada8280edb7f3b54be50dc05546f848555af2d
Salakar <mike.diarmid@gmail.com>
2018-08-08 17:04:10 +0100

    [ios] bridge -> jet

--- ios/Jet.m
+++ ios/Jet.m
@@ -1,15 +1,15 @@
-#import "RNBridge.h"
+#import "Jet.h"
 #import <objc/runtime.h>
 #import <React/RCTBridge.h>
 
 static NSString *const kRCTDevSettingIsDebuggingRemotely = @"isDebuggingRemotely";
 static NSString *const kRCTDevSettingsUserDefaultsKey = @"RCTDevMenu";
 
-@implementation RNBridge
+@implementation Jet
 
 @synthesize bridge = _bridge;
 
-RCT_EXPORT_MODULE(RNBridge)
+RCT_EXPORT_MODULE(Jet)
 
 RCT_EXPORT_METHOD(reload) {
   [_bridge reload];
