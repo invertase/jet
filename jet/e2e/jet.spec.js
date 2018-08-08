0c82e284453ba3675f778d5bab9647d3ad7421ce
Salakar <mike.diarmid@gmail.com>
2018-08-08 18:21:01 +0100

    [tests] moving timing tests to own file

--- jet/e2e/jet.spec.js
+++ jet/e2e/jet.spec.js
@@ -1,20 +1,7 @@
-const should = require('should');
 
 describe('jet', () => {
-  // beforeEach(async function beforeEach() {
-  // await device.reloadReactNative();
-  // jet.root.setState({ message: this.currentTest.title });
-  // });
-
   it('should provide -> global.jet', async () => {
     should(jet).not.be.undefined();
-    return Promise.resolve();
-  });
-
-  // main react-native module you're testing on
-  // in our case react-native-firebase
-  it('should provide -> jet.module', async () => {
-    should(jet.module).not.be.undefined();
     return Promise.resolve();
   });
 
@@ -44,7 +31,7 @@ describe('jet', () => {
 
     // test setting state
     await new Promise(resolve =>
-      jet.root.setState({ message: 'hello world' }, resolve)
+      jet.root.setState({ message: 'hello world' }, resolve),
     );
     should(jet.root.state.message).equal('hello world');
     return Promise.resolve();
@@ -76,49 +63,5 @@ describe('jet', () => {
     should(jet.rn.Platform.OS).be.a.String();
     should(jet.rn.Platform.OS).equal(device.getPlatform());
     return Promise.resolve();
-  });
-
-  // TIMERS
-  it('timing.setTimeout', cb => {
-    const start = Date.now();
-    jet.context.setTimeout(() => {
-      const timeTaken = Date.now() - start;
-      if (timeTaken >= 50) cb();
-      else cb(new Error('setTimeout fn called too soon.'));
-    }, 50);
-  });
-
-  it('timing.setInterval', cb => {
-    let times = 0;
-    let interval;
-    const start = Date.now();
-
-    interval = jet.context.setInterval(() => {
-      const timeTaken = Date.now() - start;
-
-      times++;
-      jet.context.clearInterval(interval);
-      if (times >= 2) {
-        return cb(new Error('Interval did not cancel correctly.'));
-      }
-
-      if (timeTaken < 50) {
-        return cb(new Error('setInterval fn called too soon.'));
-      }
-
-      return jet.context.setTimeout(cb, 100);
-    }, 50);
-  });
-
-  it('timing.setImmediate', cb => {
-    jet.context.setImmediate(() => cb());
-  });
-
-  it('timing.requestIdleCallback', cb => {
-    jet.context.requestIdleCallback(() => cb());
-  });
-
-  it('timing.requestAnimationFrame', cb => {
-    jet.context.requestAnimationFrame(() => cb());
   });
 });
