374ea70b2d5975e17ff6c3bce9bde560c0df4933
Mike Hardy <github@mikehardy.net>
2025-02-04 15:52:18 -0500

    test(ios): iOS tests working locally now

--- example/.jetrc.js
+++ example/.jetrc.js
@@ -34,5 +34,32 @@ module.exports = {
         console.log('After');
       },
     },
+    ios: {
+      config: {
+        // Per target config overrides...
+        // These will override in order of:
+        // ...cliFlags
+        // ...globalConfig
+        // ...targetConfig
+      },
+      /**
+       * Use this to run builds, start the application etc.
+       */
+      async before(config) {
+        const process = proc.spawnSync('npx', ['react-native', 'run-ios']);
+        if (process.error) {
+          console.log(process.stdout.toString());
+          console.log(process.stderr.toString());
+          throw new Error(`Failed to build ios. Error: ${process.error}`);
+        }
+        return config;
+      },
+      /**
+       * Use this for cleanup & teardown.
+       */
+      async after(config) {
+        console.log('After');
+      },
+    },
   },
 };
