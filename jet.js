aa3e4d7f22b87024cdf28b8593c29fbe3b80b919
Mike Hardy <github@mikehardy.net>
2025-02-04 09:24:08 -0500

    fix: nyc path subtly different with new yarn, require relative to cwd
    this should be backwards-compatible with the previous lookup style, but
    also works correctly with new yarn
    
    if not, and there is a problem (either with existing users or in the future)
    it now attempts to print out a helpful message indicating why it failed and
    where it looked

--- jet.js
+++ jet.js
@@ -1,7 +1,16 @@
 #!/usr/bin/env node
+const fs = require('fs');
+
 if (process.argv.includes('--coverage') && !process.env.NYC_CONFIG) {
-  process.argv.unshift(require.resolve('./node_modules/.bin/nyc'));
-  require('./node_modules/.bin/nyc');
+  const nycPath = `${process.cwd()}/node_modules/.bin/nyc`;
+  if (!fs.existsSync(nycPath)) {
+    throw Error(
+      `Jet: '--coverage' requested but cannot find 'nyc'. Did not run from app root? Looked in: '${nycPath}'`
+    );
+  }
+
+  process.argv.unshift(require.resolve(nycPath));
+  require(nycPath);
 } else {
   require('./lib/commonjs/cli.js');
 }
