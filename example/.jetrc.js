505d9da52ceace7e22748905d20e7108ad1c5afb
Mike Hardy <github@mikehardy.net>
2025-10-24 09:08:12 -0500

    test: add android alongside ios and macos example targets

--- example/.jetrc.js
+++ example/.jetrc.js
@@ -61,5 +61,32 @@ module.exports = {
         console.log('After');
       },
     },
+    android: {
+      config: {
+        // Per target config overrides...
+        // These will override in order of:
+        // ...cliFlags
+        // ...globalConfig
+        // ...targetConfig
+      },
+      /**
+       * Use this to run builds, start the application etc.
+       */
+      async before(config) {
+        const process = proc.spawnSync('npx', ['react-native', 'run-android']);
+        if (process.error) {
+          console.log(process.stdout.toString());
+          console.log(process.stderr.toString());
+          throw new Error(`Failed to build android. Error: ${process.error}`);
+        }
+        return config;
+      },
+      /**
+       * Use this for cleanup & teardown.
+       */
+      async after(config) {
+        console.log('After');
+      },
+    },
   },
 };
